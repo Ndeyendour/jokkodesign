9aef81f3b9cd42d2a49cbce611533dc3d91969f7
Ndeyendour <ndeyefatou023@gmail.com>
2025-02-18 22:16:59 +0000

    SALUT

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "jokkotechs@gmail.com";
+$address = "jokkodesigns@gmail.com";
 
 
 // Configuration option.
